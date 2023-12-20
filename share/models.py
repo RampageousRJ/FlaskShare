e8f9a47ef2ee3725e4308bb0fc6bf90a5a026ffd
Rishabh Jain <rishabh.jain1@learner.manipal.edu>
2023-12-20 19:47:50 +0530

    Model Update

--- share/models.py
+++ share/models.py
@@ -4,4 +4,15 @@ from flask_bcrypt import check_password_hash,generate_password_hash
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer,primary_key=True)
-    password_hash = 
+    password_hash = db.Column(db.String(128),nullable=False)
+    
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute')
+    
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
+    
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
